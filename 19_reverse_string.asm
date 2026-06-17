f18c9a2bc0de1649f81905baab6740dee168247d
Sehrish <sehrishjaved119@gmail.com>
2026-06-17 19:58:00 +0500

    Add program to reverse a string

---
+++ 19_reverse_string.asm
@@ -0,0 +1,30 @@
+;Write a program that reverse a string.
+.model small
+.stack 100h
+.data
+string db 'Sehrish'
+.code
+main proc
+mov ax, @data
+mov ds, ax
+
+lea si, string
+
+mov cx, 7
+l1:
+mov bx, [si]
+push bx
+inc si
+loop l1
+
+mov cx, 7
+l2:
+pop dx
+mov ah, 2
+int 21h
+loop l2
+
+mov ah, 4ch
+int 21h
+main endp
+end main
